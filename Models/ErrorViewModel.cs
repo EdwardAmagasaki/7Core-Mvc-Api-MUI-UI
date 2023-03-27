2a688ee60f6f5878eaeb92d7ca9d45209e6775b5
Edward Amagasaki <amagasaki.edward@gmail.com>
2023-03-27 15:20:10 -0300

    ..

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _7Core_Mvc_Api_MUI_UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
